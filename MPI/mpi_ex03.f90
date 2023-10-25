088b9ab23eded040d65addee19ca0b69e549225d
yuRina331 <runatann0331kami@gmail.com>
2023-10-25 22:40:08 +0900

    upload OpenACC result

--- MPI/mpi_ex03.f90
+++ MPI/mpi_ex03.f90
@@ -11,20 +11,15 @@ program sum_allreduce
 
   if( myrank==0)  n=10000
   call MPI_Bcast( n, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr )
-
+  
   istart =  myrank   *(n/nprocs) + 1
   iend   = (myrank+1)*(n/nprocs)
-
   isum_local = 0
   do i = istart, iend
     isum_local = isum_local + i
   enddo
 
   call MPI_Allreduce( isum_local, isum, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr )
-
-  print *, 'Rank:', myrank,  '-> Local sum = ', isum_local
-  print *, 'Rank:', myrank,  '-> Total sum = ', isum
-
   call MPI_Finalize( ierr )
   stop
 end program sum_allreduce
